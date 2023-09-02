48d8d9729549a1c6506773347261011150d5d828
Roodewald <pavel999977@gmail.com>
2023-09-02 12:59:40 +0500

    Add new layout

--- app/src/main/java/com/verter/qrstock/MainActivity.java
+++ app/src/main/java/com/verter/qrstock/MainActivity.java
@@ -14,27 +14,34 @@ import com.journeyapps.barcodescanner.ScanOptions;
 public class MainActivity extends AppCompatActivity {
 
     Button bth_scan;
+    Button bth_addProduct;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bth_scan = findViewById(R.id.bth_scan);
-        bth_scan.setOnClickListener(v-> scanCode());
+        bth_addProduct = findViewById((R.id.bth_addProduct));
+        bth_scan.setOnClickListener(v -> scanCode());
+        bth_addProduct.setOnClickListener(v -> addView());
+    }
+
+    private void addView(){
+        setContentView(R.layout.add_product_layout);
     }
 
     private void scanCode() {
         ScanOptions options = new ScanOptions();
-        options.setPrompt("Volume up to flash on");
+        options.setPrompt("Громкость вверх (фонарик)");
         options.setBeepEnabled(true);
         options.setOrientationLocked(true);
         options.setCaptureActivity(CaptureAct.class);
         barLauncher.launch(options);
     }
-    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(),result -> {
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
         if(result.getContents() != null){
             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
             builder.setTitle("Result");
-            builder.setMessage(result.getContents());
+            builder.setMessage("Result " + result.getContents());
             builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
